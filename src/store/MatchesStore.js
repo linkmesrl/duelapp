cbc6137ea02f3ad66899194cd0ba00a26405b777
ilgianfra <gianfranco.garipoli@gmail.com>
2016-08-05 17:27:16 +0200

    fix firebase api of get matches

--- src/store/MatchesStore.js
+++ src/store/MatchesStore.js
@@ -20,14 +20,14 @@ class MatchesStore {
   }
 
   @action getMatchesList() {
-    this.matchesList = FirebaseAPI.getMatches();
-      // .then(res => {
-      //   console.log('getMatchesList', res.val());
-      //   this.matchesList = res.val();
-      // })
-      // .catch((err) => {
-      //   console.log('Error getting matches: ', err);
-      // });
+    FirebaseAPI.getMatches()
+      .then(res => {
+        console.log('getMatchesList', res.val());
+        this.matchesList = res.val();
+      })
+      .catch((err) => {
+        console.log('Error getting matches: ', err);
+      });
   }
 }
 
--- src/util/FirebaseAPI.js
+++ src/util/FirebaseAPI.js
@@ -28,8 +28,8 @@ export default FirebaseAPI = {
   },
 
   getMatches() {
-    return firebase.database().ref('/matches').once('value')
-      .then((snapshot) => snapshot.val());
+    return firebase.database().ref('/matches').once('value');
+      // .then((snapshot) => snapshot.val());
   },
 
   login(username, password) {
